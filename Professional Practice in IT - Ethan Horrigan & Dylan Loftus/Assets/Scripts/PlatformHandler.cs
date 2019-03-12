754c4448b8701d14949aee03b85efb1cb643cb06
EthanHorrigan <ethanhorr@gmail.com>
2019-03-12 21:07:58 +0000

    Initial Commit

---
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/PlatformHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformHandler : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject platform;
+    BoxCollider2D coll;
+    public string platformId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPlayer(player);
+        Debug.Log(player.tag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetPlayer(GameObject p)
+    {
+        player = p;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" && platformId == "Player2")
+        {
+           Debug.Log("PLAYER 1 COLLIDED");
+            platform.GetComponent<BoxCollider2D>().enabled = false;
+            Debug.Log("REEEEEE");
+        }
+
+        if (collision.gameObject.tag == "Player2" && platformId == "Player1")
+        {
+            Debug.Log("PLAYER 2 COLLIDED");
+            platform.GetComponent<BoxCollider2D>().enabled = false;
+        }
+
+
+    }
+}
---
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float movementSpeed = 3f;
+    float jumpSpeed = 800f;
+    bool isGrounded = true;
+    Rigidbody2D rb;
+    public string playerId;
+
+
+
+    void Start()
+    {
+        SetPlayerId(playerId);
+        Debug.Log(playerId);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetPlayerId(string id)
+    {
+        playerId = id;
+    }
+
+    public string GetPlayerId()
+    {
+        return playerId;
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            rb.gravityScale = 2.1f;
+        }
+
+        if (playerId == "Player1")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (isGrounded)
+                {
+                    rb.AddForce(Vector3.up * jumpSpeed);
+                    isGrounded = false;
+                }
+            }
+        }
+
+        if (playerId == "Player2")
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (isGrounded)
+                {
+                    rb.AddForce(Vector3.up * jumpSpeed);
+                    isGrounded = false;
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (!isGrounded)
+                {
+                    rb.gravityScale += 2;
+                }
+            }
+        }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
