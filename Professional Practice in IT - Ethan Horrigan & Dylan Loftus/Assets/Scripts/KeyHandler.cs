57df74ac58f493ebbbf9a455c01d43d83f93e3ab
EthanHorrigan <ethanhorr@gmail.com>
2019-04-03 01:12:36 +0100

    New Scene & Collect Sound

--- Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/KeyHandler.cs
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/KeyHandler.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class KeyHandler : MonoBehaviour
 {
+    public AudioClip collect;    // Add your Audi Clip Here;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = collect;
+    }
 
     public void OnTriggerEnter2D(Collider2D node)
        {
            if (node.gameObject.tag == "PickUp")
            {
-               node.gameObject.SetActive(false);
+            node.gameObject.SetActive(false);
             Health.keysCollected++;
-           }
+            GetComponent<AudioSource>().Play();
+        }
        }
 }
