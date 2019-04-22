e10422d278086eaec3e69a930c6933bf9d0ee589
EthanHorrigan <ethanhorr@gmail.com>
2019-04-22 22:09:18 +0100

    Level Selection UI

--- Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/InterfaceController.cs
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/InterfaceController.cs
@@ -3,10 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InterfaceController : MonoBehaviour
 {
 
+    public Button PlayButton;
+
+    private string SelectLevelScene = "LevelSelector";
+    
+    private void Start()
+    {
+        PlayButton.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    private IEnumerator LoadScene()
+    {
+        //show animate out animation
+        //animator.SetBool("animateOut", true);
+        yield return new WaitForSeconds(1f);
+
+        //load the scene we want
+        SceneManager.LoadScene(SelectLevelScene);
+    }
 }
 
 
---
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LevelHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject highlight;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("MouseEnter");
+        highlight.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("MouseExit");
+        highlight.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
