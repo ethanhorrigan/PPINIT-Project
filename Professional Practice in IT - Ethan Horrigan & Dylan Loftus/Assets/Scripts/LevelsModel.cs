2b1b9ad0fee471f93fa09875d82344838c69f8f0
EthanHorrigan <ethanhorr@gmail.com>
2019-04-23 01:57:22 +0100

    Finished UI for LevelSelector

---
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/LevelsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    class LevelsModel
+    {
+        public int CurrentLevel { get; set; }
+        public int[] Levels { get; set; }
+    }
+}
