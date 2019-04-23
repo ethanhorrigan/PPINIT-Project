83a7a5415ea75b587b2046d4785c767f41d5bb5a
GoodVibes0nly <dylanloftus@hotmail.com>
2019-04-23 17:53:00 +0100

    tidy up

--- Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/KeyHandler.cs
+++ Professional Practice in IT - Ethan Horrigan & Dylan Loftus/Assets/Scripts/KeyHandler.cs
@@ -13,12 +13,12 @@ public class KeyHandler : MonoBehaviour
     }
 
     public void OnTriggerEnter2D(Collider2D node)
-       {
-           if (node.gameObject.tag == "PickUp")
-           {
+    {
+        if (node.gameObject.tag == "PickUp")
+        {
             node.gameObject.SetActive(false);
             Health.keysCollected++;
             GetComponent<AudioSource>().Play();
         }
-       }
+    }
 }
